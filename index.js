0e6347ea25267561e5aefaccaefdaf3f6222da62
ronald vilorio <soccer23415@gmail.com>
2018-05-21 01:35:58 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ function produceDrivingRange(blockRange){
     // debugger
     start = parseInt(start.slice(0,2))
     end = parseInt(end.slice(0,2))
-    
-    if (end - start  > blockRange){
+
+    if (end - start  < blockRange){
       console.log(`${end - start} blocks out of range`)
     }else{
       console.log(`within range by ${end - start}`)
