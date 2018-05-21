776a67858eaacfab3b046e8b3a552a9c638ac7a7
ronald vilorio <soccer23415@gmail.com>
2018-05-21 01:59:08 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,7 +7,6 @@ function produceDrivingRange(blockRange){
 
     if (blockRange < (end - start)){
        console.log(`${(end - start) - blockRange} blocks out of range`)
-
     }else{
        console.log(`within range by ${(end - start)}`)
     }
