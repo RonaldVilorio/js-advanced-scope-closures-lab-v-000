dd19a8dc16e42363b231f75638199aad793a8eb4
ronald vilorio <soccer23415@gmail.com>
2018-05-21 02:07:04 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,20 @@
+
+ function produceDrivingRange(blockRange){
+  return function(start,end){
+    // debugger
+    start = parseInt(start.slice(0,2))
+    end = parseInt(end.slice(0,2))
+
+    if (blockRange < (end - start)){
+       return `${(end - start) - blockRange} blocks out of range`
+    }else{
+       return `within range by ${(end - start)}`
+    }
+  }
+}
+function produceTipCalculator(tip_percent){
+  return function(tip){
+    return tip * tip_percent
+  }
+
+}
