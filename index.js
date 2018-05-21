d9d09ee4a5aa58ae3cbfd6566a8a047abaa25e28
ronald vilorio <soccer23415@gmail.com>
2018-05-21 02:02:13 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 
-function produceDrivingRange(blockRange){
+let a = function produceDrivingRange(blockRange){
   return function(start,end){
     // debugger
     start = parseInt(start.slice(0,2))
