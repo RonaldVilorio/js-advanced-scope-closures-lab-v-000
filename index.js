becfaba7369c5437fc835a5fb00b0a5db3acb3db
ronald vilorio <soccer23415@gmail.com>
2018-05-21 01:32:13 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 
 function produceDrivingRange(blockRange){
   return function(start,end){
-    debugger
+    // debugger
     start = parseInt(start.slice(0,2))
     end = parseInt(end.slice(0,2))
-    if (end - start  > blockRange)
-    console.log(start)
+    if (end - start  > blockRange){
+      console.log(``)
+    }
+
 
 
   }
